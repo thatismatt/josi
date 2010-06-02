1d786fc322a959a028e7450c6326c1d9f46bd503
thatismatt <thatismatt@gmail.com>
2010-06-02 22:41:34 +0100

    Route the static content directly, in the hello example.

--- examples/hello/app.js
+++ examples/hello/app.js
@@ -3,4 +3,5 @@ this.init = function() {
     /^\/(?:(\w+)\/?)?(?:(\w+)\/?)?(?:(\w+)\/?)?$/,
     { controller: 'home', action: 'index' }
   );
+  this.router.addStatic('static');
 };
--- examples/hello/controllers/static.js
+++
@@ -1,6 +0,0 @@
-var content = require('josi/actionresults').content;
-
-this.css = function() { return content('static/style.css'); };
-this.jquery = function() { return content('static/jquery-1.4.2.min.js'); };
-this.myjs = function() { return content('static/my.js'); };
-this.favicon = function() { return content('static/favicon.png'); };
