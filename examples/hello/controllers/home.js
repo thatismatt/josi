63c5192487667cd7cf2c672ed326fc9c94b9eb88
thatismatt <thatismatt@gmail.com>
2010-06-03 18:37:44 +0100

    Rebranding, josi is always written all lowercase.

--- examples/hello/controllers/home.js
+++ examples/hello/controllers/home.js
@@ -2,5 +2,7 @@ var actionresults = require('josi/actionresults');
 var redirect = actionresults.redirect;
 var view = actionresults.view;
 
+a.fail();
+
 this.index = function() { return view({ title: 'A Title', name: 'Kate' }); };
 this.redir = function() { return redirect('/product/details/1'); }
