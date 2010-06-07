2573353bce91e54bd4bd61a950e539f4c769508b
thatismatt <thatismatt@gmail.com>
2010-06-07 12:11:09 +0100

    Removed some debug code.

--- examples/hello/controllers/home.js
+++ examples/hello/controllers/home.js
@@ -2,7 +2,5 @@ var actionresults = require('josi/actionresults');
 var redirect = actionresults.redirect;
 var view = actionresults.view;
 
-a.fail();
-
 this.index = function() { return view({ title: 'A Title', name: 'Kate' }); };
 this.redir = function() { return redirect('/product/details/1'); }
