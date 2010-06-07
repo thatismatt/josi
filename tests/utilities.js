901b7c5687cfa20dec87b109ec775722ff5b9f9f
thatismatt <thatismatt@gmail.com>
2010-06-07 19:34:30 +0100

    Separate the notion of command line arguments that are key value pairs (opts) and those that are just a value (args), and some tests.

--- tests/utilities.js
+++ tests/utilities.js
@@ -19,5 +19,25 @@ this.tests = {
   'Other dots in file ignored': function() {
     var result = utilities.stripExtension('index.2.html');
     assert.equal(result, 'index.2');
+  },
+  'First argument is task': function() {
+    var processed = utilities.processARGV(['', '', 'atask']);
+    assert.equal(processed.task, 'atask');
+  },
+  'Dashes are ignored': function() {
+    var processed = utilities.processARGV(['', '', 'atask', '-a=b']);
+    assert.equal(processed.opts.a, 'b');
+  },
+  'Double dashes are ignored': function() {
+    var processed = utilities.processARGV(['', '', 'atask', '--a=b']);
+    assert.equal(processed.opts.a, 'b');
+  },
+  'Dashes not required': function() {
+    var processed = utilities.processARGV(['', '', 'atask', 'a=b']);
+    assert.equal(processed.opts.a, 'b');
+  },
+  'If not key value pair then it is an arg': function() {
+    var processed = utilities.processARGV(['', '', 'atask', 'arg']);
+    assert.equal(processed.args[0], 'arg');
   }
 };
