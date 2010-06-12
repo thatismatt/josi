5b5dc2286f8299e8563d8e53dbba0e40030625d3
thatismatt <thatismatt@gmail.com>
2010-06-12 15:21:51 +0100

    Can now specify which view to render. Show error page if template rendering errors.

--- examples/hello/controllers/home.js
+++ examples/hello/controllers/home.js
@@ -2,5 +2,20 @@ var actionresults = require('josi/actionresults');
 var redirect = actionresults.redirect;
 var view = actionresults.view;
 
-this.index = function() { return view({ title: 'A Title', name: 'Kate' }); };
-this.redir = function() { return redirect('/product/details/1'); }
+this.index = function() {
+  return view({
+    title: 'A Title',
+    details: 'Some details'
+  });
+};
+
+this.another = function() {
+  return view({
+    title: 'Another Title',
+    details: 'A different action, using the same view.'
+  }, 'index');
+};
+
+this.redir = function() {
+  return redirect('/product/details/1');
+};
