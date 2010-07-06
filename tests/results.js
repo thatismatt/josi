5bb8cf99a0c5713cf8573ae7d53e8c39a3414156
thatismatt <thatismatt@gmail.com>
2010-07-06 14:28:02 +0100

    Added basic cookie functionality.

--- tests/results.js
+++ tests/results.js
@@ -3,13 +3,30 @@ var actionresults = require('josi/actionresults');
 
 this.name = 'Results Tests';
 
+var getHeader = function(headers, key) {
+  for (var i in headers) {
+    var header = headers[i];
+    if (header[0] === key) {
+      return header[1];
+    }
+  }
+};
+
 this.tests = {
   'Content Type of view is HTML': function() {
     var result = actionresults.view({});
-    assert.equal(result.headers['Content-Type'], 'text/html');
+    assert.equal(getHeader(result.headers, 'Content-Type'), 'text/html');
   },
   'Content Type of json is json': function() {
     var result = actionresults.json({});
-    assert.equal(result.headers['Content-Type'], 'application/json');
+    assert.equal(getHeader(result.headers, 'Content-Type'), 'application/json');
+  },
+  'Content Type of raw is plain text': function() {
+    var result = actionresults.raw({});
+    assert.equal(getHeader(result.headers, 'Content-Type'), 'text/plain');
+  },
+  'Content Type of error is plain text': function() {
+    var result = actionresults.error({});
+    assert.equal(getHeader(result.headers, 'Content-Type'), 'text/plain');
   },
 };
