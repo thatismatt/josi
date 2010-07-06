ce09c604f6948d18662f9a1f0c8d600e178146c5
thatismatt <thatismatt@gmail.com>
2010-07-06 14:29:05 +0100

    Added example usage of cookies to hello app.

--- examples/hello/controllers/home.js
+++ examples/hello/controllers/home.js
@@ -19,3 +19,15 @@ this.another = function() {
 this.redir = function() {
   return redirect('/product/details/1');
 };
+
+this.cookie = function() {
+  var aCookie = this.cookie.get('a') === 'red' ? 'blue' : 'red';
+  var bCookie = (parseInt(this.cookie.get('b'), 10) || 0) + 1;
+  this.cookie.set('a', aCookie);
+  this.cookie.set('b', bCookie);
+  return view({
+    title: 'Cookies',
+    a: aCookie,
+    b: bCookie
+  });
+};
